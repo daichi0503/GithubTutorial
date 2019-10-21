fcc097fd0a17ce6b0491b5d7d10782968351556e
daichi0503 <is0433sk@ed.ritsumei.ac.jp>
2019-10-21 17:17:36 +0900

    add king matsuzaka

--- src/main.py
+++ src/main.py
@@ -7,8 +7,11 @@ while True:
     if input_num == 0:
         print('プログラムを終了します')
         break
-    elif input_num > 0:
+    elif input_num > 0 and input_num <= 10:
         print("Hello World")
+        break
+    elif input_num > 10:
+        print("king matsuzaka")
         break
     # -- ここに処理を追加してみよう --
     else:
